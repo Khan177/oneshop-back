03477b0f792c42d92426c5fc440ceccef02d86af
Adilkhan Nurgaliyev <khan17171717@gmail.com>
2020-07-28 15:15:44 +0600

    initital

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PORT = process.env.PORT;
+const DB_KEY = process.env.DB_KEY;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(DB_KEY, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running...");
+});
