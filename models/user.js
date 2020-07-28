e16e0838168c75f906f16c0f9343eda845e40378
User <180103284@stu.sdu.edu.kz>
2020-07-28 16:38:58 +0600

    changed User model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  title: {
+  name: {
     type: String,
     required: true,
-  }
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model('User', UserSchema);
