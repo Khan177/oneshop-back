1c03d7f20286ab38ed4960b17a1581b80f5ee216
User <180103284@stu.sdu.edu.kz>
2020-07-30 15:31:50 +0600

    added qa

---
+++ models/qa.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const QASchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+});
+
+const QA = mongoose.model('QA', QASchema);
+module.exports = QA;
