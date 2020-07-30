f376670b792844dea70ee780338edf7607d275d8
User <180103284@stu.sdu.edu.kz>
2020-07-30 15:07:23 +0600

    added direction module

---
+++ models/direction.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const DirectionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  }
+});
+
+const DirectionModel = mongoose.model('Direction', DirectionSchema);
+module.exports = DirectionModel;
--- router/orderRouter.js
+++ router/orderRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productModel = require('../models/product');
+const productModel = require('../models/direction');
 const orderModel = require('../models/order');
 
 router
