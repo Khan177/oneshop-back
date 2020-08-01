ddd839d6510492b12a59e70a6a1782fc59f3517b
User <180103284@stu.sdu.edu.kz>
2020-08-01 16:56:02 +0600

    uploading files

--- index.js
+++ index.js
@@ -8,16 +8,111 @@ const DB_KEY = process.env.DB_KEY;
 const directionRouter = require('./router/directionRouter');
 const qaRouter = require('./router/qaRouter');
 
+const GridFSStorage = require('multer-gridfs-storage');
+const GridFS = require('gridfs-stream');
+const multer = require('multer');
+const crypto = require('crypto');
+var gfs;
+
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(DB_KEY, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then((conn) => {
+  gfs = GridFS(conn.connection.db, mongoose.mongo);
+  gfs.collection('uploads')
 });
 
 app.use('/directions', directionRouter);
 app.use('/qa', qaRouter);
+
+const storage = new GridFSStorage({
+  url: DB_KEY,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+app.get('/images', async (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    if (!files) {
+      return res.status(404).json({
+        err: 'No files found'
+      });
+    }
+    files.map(file => {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    })
+  });
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+});
+
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    if (!files) {
+      return res.status(404).json({
+        err: 'No files found'
+      });
+    }
+    return res.json(files);
+  });
+});
+
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    if (!file) {
+      return res.status(404).json({
+        err: 'No file found'
+      });
+    }
+    return res.json(file);
+  });
+});
+
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    if (!file) {
+      return res.status(404).json({
+        err: 'No file found'
+      });
+    }
+    if (file.contentType === 'image/png' || file.contentType === 'image/jpeg') {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+app.delete('/files/:filename', (req, res) => {
+  gfs.remove({ filename: req.params.filename, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+  })
+});
 
 app.listen(PORT, () => {
   console.log('Server is running...');
